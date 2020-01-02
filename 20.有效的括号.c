b14e5f210ff9d110f687eb644f2a90773ed92a30
Qin-Hao <qh530658481@outlook.com>
2020-01-02 20:41:46 +0800

    11

---
+++ 20.有效的括号.c
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=c
+ *
+ * [20] 有效的括号
+ */
+
+// @lc code=start
+
+
+bool isValid(char * s)
+{
+    if (s == "")
+    {
+        return true;
+    }
+    int length = strlen(s);
+    for (int i = 0; i < length; i++)
+    {
+        
+    }
+}
+
+
+// @lc code=end
+
